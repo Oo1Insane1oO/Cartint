ab3e6ef263ab4d736b73df2f30b75672efc15f3c
Alocias Mariadason <alocia0@gmail.com>
2019-10-12 18:33:11 +0200

    fixed num_basis bug, changed name to cut_off instead

--- interface/main.cpp
+++ interface/main.cpp
@@ -6,15 +6,17 @@
 
 int main(int argc, char *argv[]) {
     unsigned int dim = std::atoi(argv[1]);
-    unsigned int num_basis = std::atoi(argv[2]) / 2;
+    unsigned int cut_off = std::atoi(argv[2]);
     double scaling = std::atof(argv[3]);
     double omega = std::atof(argv[4]);
 
-    GaussianIntegrals* I = new GaussianIntegrals(dim, num_basis, scaling);
+    GaussianIntegrals* I = new GaussianIntegrals(dim, cut_off, scaling);
     I->initializeParameters(omega);
 
+    double num_basis = cut_off / 2;
     for (unsigned int p = 0; p < num_basis; ++p) {
         for (unsigned int q = 0; q < num_basis; ++q) {
+                    // std::cout << std::setprecision(15) << p << " " << q << " " << I->oneBodyElement(p, q) << std::endl;
             for (unsigned int r = 0; r < num_basis; ++r) {
                 for (unsigned int s = 0; s < num_basis; ++s) {
                     std::cout << std::setprecision(15) << p << " " << q << " " << r << " " << s << " " << I->coulombElement(p, q, r, s) << std::endl;
@@ -23,19 +25,6 @@ int main(int argc, char *argv[]) {
         } // end forq
     } // end forp
 
-    std::vector<double> eigs;
-    for (unsigned int i = 0; i < num_basis; ++i) {
-        for (unsigned int j = i; j < num_basis; ++j) {
-            double el = I->oneBodyElement(i, j);
-            if (el > 1e-15) {
-                eigs.push_back(I->oneBodyElement(i, j));
-            }
-        } // end forj
-    } // end fori
-
-    for (auto i : eigs) {
-        std::cout << i << std::endl;
-    }
 
     delete I;
     return 0;
