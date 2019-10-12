f9c36e793660f77fd0f058f639104db7a0b1065e
Alocias Mariadason <alocia0@gmail.com>
2019-10-12 15:44:47 +0200

    added argv

--- interface/main.cpp
+++ interface/main.cpp
@@ -1,5 +1,6 @@
 #include <iomanip>
 #include <cstdlib>
+#include <vector>
 
 #include <cartint.hpp>
 
@@ -21,6 +22,20 @@ int main(int argc, char *argv[]) {
             } // end forr
         } // end forq
     } // end forp
+
+    std::vector<double> eigs;
+    for (unsigned int i = 0; i < num_basis; ++i) {
+        for (unsigned int j = i; j < num_basis; ++j) {
+            double el = I->oneBodyElement(i, j);
+            if (el > 1e-15) {
+                eigs.push_back(I->oneBodyElement(i, j));
+            }
+        } // end forj
+    } // end fori
+
+    for (auto i : eigs) {
+        std::cout << i << std::endl;
+    }
 
     delete I;
     return 0;
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-mpirun --oversubscribe -np 8 ./build/Cartint 2 2 1.0 1.0 > w1_d2_L2.txt
-mpirun --oversubscribe -np 8 ./build/Cartint 2 6 1.0 1.0 > w1_d2_L6.txt
-mpirun --oversubscribe -np 8 ./build/Cartint 2 12 1.0 1.0 > w1_d2_L12.txt
-mpirun --oversubscribe -np 8 ./build/Cartint 2 20 1.0 1.0 > w1_d2_L20.txt
-mpirun --oversubscribe -np 8 ./build/Cartint 2 30 1.0 1.0 > w1_d2_L30.txt
-mpirun --oversubscribe -np 8 ./build/Cartint 2 42 1.0 1.0 > w1_d2_L42.txt
+./build/Cartint 2 2 1.0 1.0 > w1_d2_L2.txt
+./build/Cartint 2 6 1.0 1.0 > w1_d2_L6.txt
+./build/Cartint 2 12 1.0 1.0 > w1_d2_L12.txt
+./build/Cartint 2 20 1.0 1.0 > w1_d2_L20.txt
+./build/Cartint 2 30 1.0 1.0 > w1_d2_L30.txt
+./build/Cartint 2 42 1.0 1.0 > w1_d2_L42.txt
